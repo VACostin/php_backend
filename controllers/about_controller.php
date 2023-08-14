f1326bf17e992a1bfec823a827cbaf7c2b3655c6
Costin <virlan.costin.andrei@gmail.com>
2023-08-14 03:31:00 +0300

    Removed example controllers'

--- controllers/about_controller.php
+++
@@ -1,5 +0,0 @@
-<?php
-$aboutController = function ($request, $response) {
-    $user = $_SESSION['user'];
-    $response->send("This is the about page. User: $user");
-};
--- controllers/home_controller.php
+++
@@ -1,5 +0,0 @@
-<?php
-$homeController = function ($request, $response) {
-    $_SESSION['user'] = 'John';
-    $response->send("Hello, this is the home page!");
-};
